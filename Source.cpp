7e5ccf2140ed2fade5f4a10e4ad3893f3daba91c
Vitaliy Klimov <vitalik_klimov@ukr.net>
2019-05-12 23:44:20 +0300

    test

--- Source.cpp
+++ Source.cpp
@@ -35,24 +35,12 @@ template<class AType> AType binarySearch(AType* m, int l, int r, int x)
 {
 	if (r >= l) {
 		int mid = l + (r - l) / 2;
-
-		// If the element is present at the middle 
-		// itself 
 		if (m[mid] == x)
 			return mid;
-
-		// If element is smaller than mid, then 
-		// it can only be present in left subarray 
 		if (m[mid] > x)
 			return binarySearch(m, l, mid - 1, x);
-
-		// Else the element can only be present 
-		// in right subarray 
 		return binarySearch(m, mid + 1, r, x);
 	}
-
-	// We reach here when element is not 
-	// present in array 
 	return -1;
 }
 
@@ -88,7 +76,6 @@ int main()
 	//сортування масивів 
 //	sort(intarray, 5);
 //	sort(B, 7);
-	// затримка
 	int x = 3;
 	int result = binarySearch(intarray, 0, 4, x);
 	cout << "\n result = " << result << endl;
